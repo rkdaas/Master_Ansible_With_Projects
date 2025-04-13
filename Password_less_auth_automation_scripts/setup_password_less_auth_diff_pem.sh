de39303ef080b701772c7b5a798470ce952df665
rkdaas <gupta.ritik2002@gmail.com>
2025-04-13 10:47:29 +0000

    Assignment Task-2 and Task-3 completed

--- Password_less_auth_automation_scripts/setup_password_less_auth_diff_pem.sh
+++ Password_less_auth_automation_scripts/setup_password_less_auth_diff_pem.sh
@@ -3,7 +3,8 @@
 Pub_key="home/rkdaas/my-key.pub"
 csv_file="servers.csv"
 
-while IFS=, read -r ip pem; do 
+while IFS=, read -r ip user || [ -n "$ip" ]; do
 	echo "copying key to $ip using $pem"
 	ssh-copy-id -f -i $Pub_key -o "IdentityFile=$pem" ubuntu@$ip
 done<"$csv_file"
+
